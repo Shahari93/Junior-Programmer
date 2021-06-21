9883ba712b5c4f1b7e06c646014d756a839b8652
Shahari93 <shaharilani93@gmail.com>
2021-06-21 21:24:18 +0300

    Unit 2 project

--- Unit 1/Unit 1/Assets/_Unit1/Scripts/Misc/FollowPlayerSplitScreen.cs
+++ Unit 1/Unit 1/Assets/_Unit1/Scripts/Misc/FollowPlayerSplitScreen.cs
@@ -6,7 +6,6 @@ public class FollowPlayerSplitScreen : MonoBehaviour
 {
     public GameObject player;
     [SerializeField] private Vector3 offset;
-
     //Late update accours after the main update runs
     void LateUpdate()
     {
