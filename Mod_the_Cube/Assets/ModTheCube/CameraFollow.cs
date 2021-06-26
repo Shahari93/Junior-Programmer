0b526b34370f8eeef1d0a33e7b5ad52a296e5651
Shahari93 <50714305+Shahari93@users.noreply.github.com>
2021-06-26 12:32:12 +0300

    WebGL build for mod the cube challenge

---
+++ Mod_the_Cube/Assets/ModTheCube/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 offset = new Vector3(11,11,11);
+    public GameObject cube;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        mainCamera.transform.position = cube.transform.position + offset;
+    }
+}
--- Mod_the_Cube/Assets/ModTheCube/Cube.cs
+++ Mod_the_Cube/Assets/ModTheCube/Cube.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    [SerializeField] float randomFactor;
+    [SerializeField] float rotationFactor;
+    [SerializeField] float rColorFactor;
+    [SerializeField] float bColorFactor;
+    [SerializeField] float gColorFactor;
+    [SerializeField] float aColorFactor;
+    private Material material;
+    public float speed = 1.0f;
+    float startTime;
+
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        startTime = Time.time;
+        randomFactor = Random.Range(0f, 5f);
+        rotationFactor = Random.Range(-2f, 4f);
+        rColorFactor = Random.Range(0f, 1f);
+        bColorFactor = Random.Range(0f, 1f);
+        gColorFactor = Random.Range(0f, 1f);
+        aColorFactor = Random.Range(0f, 1f);
+        transform.position = new Vector3(randomFactor, randomFactor, randomFactor);
+        transform.localScale = Vector3.one * randomFactor;
+
+        material = Renderer.material;
+
+        material.color = new Color(rColorFactor, gColorFactor, bColorFactor, aColorFactor);
     }
-    
+
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(rotationFactor * Time.deltaTime, rotationFactor, rotationFactor);
+        float t = (Mathf.Sin(Time.time - startTime) * speed);
+        material.color = Color.Lerp(Color.white, material.color, t);
     }
 }
