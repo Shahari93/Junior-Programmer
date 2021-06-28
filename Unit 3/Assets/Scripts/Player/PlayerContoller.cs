fbe756fc08c5876c24a995ffe325949e68174345
Shahari93 <shaharilani93@gmail.com>
2021-06-28 23:47:10 +0300

    Particles and Sound & SFX for endless runner

--- Unit 3/Assets/Scripts/Player/PlayerContoller.cs
+++ Unit 3/Assets/Scripts/Player/PlayerContoller.cs
@@ -3,20 +3,28 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    [Header("Component")]
+    public bool isGameOver;
+    [Header("Particles")]
+    public ParticleSystem explosionParticles;
+    public ParticleSystem dirtParticles;
+
+    [Header("Audio")]
+    public AudioClip[] sfx = new AudioClip[2];
+    public AudioSource playerAudio;
+
     private Rigidbody playerRB;
     private Animator playerAnim;
 
-    private bool jump = false;
-    private bool isOnGround = true;
-    public bool isGameOver;
     [Header("Jump variables")]
     [SerializeField] private float jumpForce = 10;
     [SerializeField] private float gravityModifer; // control how fast the player will fall to the ground
+    private bool jump = false;
+    private bool isOnGround = true;
 
     void Start()
     {
         // Getting the player components
+        playerAudio = GetComponent<AudioSource>();
         playerRB = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifer;
@@ -36,11 +44,18 @@ public class PlayerContoller : MonoBehaviour
         // The jump mechanics
         if (jump)
         {
-            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            playerAnim.SetTrigger("Jump_trig"); // The Trigger type is appropriate for representing an action that will occur for a short period
-            jump = false;
-            isOnGround = false;
+            PlayerJump();
         }
+    }
+
+    private void PlayerJump()
+    {
+        playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        playerAnim.SetTrigger("Jump_trig"); // The Trigger type is appropriate for representing an action that will occur for a short period
+        jump = false;
+        isOnGround = false;
+        dirtParticles.Stop();
+        playerAudio.PlayOneShot(sfx[1], 0.9f);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -48,14 +63,22 @@ public class PlayerContoller : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticles.Play();
         }
 
-        else if(collision.gameObject.CompareTag("Obstacles"))
+        else if (collision.gameObject.CompareTag("Obstacles"))
         {
-            Debug.Log("Game Over!");
-            isGameOver = true;
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
+            ObstaclesCollision();
         }
+    }
+
+    private void ObstaclesCollision()
+    {
+        isGameOver = true;
+        playerAnim.SetBool("Death_b", true);
+        playerAnim.SetInteger("DeathType_int", 1);
+        explosionParticles.Play();
+        dirtParticles.Stop();
+        playerAudio.PlayOneShot(sfx[0], 0.9f);
     }
 }
